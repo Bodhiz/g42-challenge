69a4df94d753afe0a51856c4fe010e5deb74bc55
Jonathan Adeline <jadeline@doubletrade.com>
2019-05-06 23:15:33 +0200

    Initial commit

---
+++ build/precache-manifest.ae3539cf7e648bb0aee20922673ee3f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44edd61ef9f5498fa70d3f598e4aa66e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "675fe524e0ff68c00e10",
+    "url": "/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "edfa02e29e67176d3ce2",
+    "url": "/static/css/main.c853d96a.chunk.css"
+  },
+  {
+    "revision": "675fe524e0ff68c00e10",
+    "url": "/static/js/2.33d6c8b5.chunk.js"
+  },
+  {
+    "revision": "edfa02e29e67176d3ce2",
+    "url": "/static/js/main.220c229e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
